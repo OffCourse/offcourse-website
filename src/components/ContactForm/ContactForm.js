fcf194840c10be7aa87fe46e6d4c1f17063f3cc3
Joris de Ruiter <joris@stay-bold.com>
2018-03-09 16:59:59 +0100

    Add Collaborate page

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Button } from 'offcourse-ui-components';
+
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      email: '',
+      message: '',
+      name: '',
+    };
+  }
+
+  handleInputChange(ev) {
+    const { name, value } = ev.target;
+
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit() {
+    alert('We need to submit contact form');
+
+    this.setState({
+      email: '',
+      message: '',
+      name: '',
+    });
+  }
+
+  render() {
+    const { email, message, name } = this.state;
+
+    return (
+      <form className="ui form" onSubmit={this.handleSubmit}>
+        <div className="field">
+          <label htmlFor="name">Name</label>
+          <input
+            name="name"
+            id="name"
+            onChange={this.handleInputChange}
+            type="text"
+            value={name}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="email">Name</label>
+          <input
+            name="email"
+            id="email"
+            onChange={this.handleInputChange}
+            type="email"
+            value={email}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            onChange={this.handleInputChange}
+            value={message}
+          />
+        </div>
+
+        <Button type="submit">Submit message</Button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
---
+++ src/pages/collaborate.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import Img from 'gatsby-image';
+
+import CallToAction from '../components/CallToAction';
+import ContactForm from '../components/ContactForm';
+import PageHeader from '../components/PageHeader';
+import Section from '../components/Section';
+
+
+const Collaborate = (props) => {
+  const { data } = props;
+  const partners = [
+    'codeRotterdam',
+    'jinc',
+    'microsoft',
+    'newDutchSchool',
+    'rawdata',
+    'rotterdamPartners',
+    'waag',
+    'wetech',
+  ];
+
+  return (
+    <div>
+      <PageHeader
+        title="Collaborate"
+        subtitle="Why you should collaborate with us"
+      />
+      <Section>
+        <div className="ui middle aligned grid container">
+          <div className="centered row">
+            <div className="ten wide column">
+              <p>WIP</p>
+            </div>
+          </div>
+        </div>
+      </Section>
+      <Section
+        title="Partners that are already in for the ride"
+      >
+        <div className="ui grid container">
+          <div className="middle aligned row">
+
+            {partners.map((partner) => {
+              const image = data[partner];
+
+              return (
+                <div className="two wide column">
+                  <Img
+                    sizes={image.sizes}
+                  />
+                </div>
+              );
+            })}
+
+          </div>
+        </div>
+      </Section>
+
+      <Section backgroundColor="offwhite">
+        <CallToAction
+          action={(
+            <div className="ui centered row">
+              <div className="ten wide column">
+                <ContactForm />
+              </div>
+            </div>
+          )}
+          title="Contact us if you want to get in contact with us"
+
+        />
+      </Section>
+    </div>
+  );
+};
+
+export default Collaborate;
+
+export const query = graphql`
+  query CollaborateImageQuery {
+    codeRotterdam: imageSharp(id: { regex: "/../assets/img/logos/code_rotterdam.png/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    jinc: imageSharp(id: { regex: "/../assets/img/logos/jinc.png/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    microsoft: imageSharp(id: { regex: "/../assets/img/logos/microsoft.png/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    newDutchSchool: imageSharp(id: { regex: "/../assets/img/logos/new_dutch_school.png/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    rawdata: imageSharp(id: { regex: "/../assets/img/logos/rawdata.jpeg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    rotterdamPartners: imageSharp(id: { regex: "/../assets/img/logos/rotterdam_partners.jpeg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    waag: imageSharp(id: { regex: "/../assets/img/logos/waag.png/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    wetech: imageSharp(id: { regex: "/../assets/img/logos/wetech.png/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+  }
+`;
