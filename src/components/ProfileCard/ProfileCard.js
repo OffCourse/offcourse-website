44f7b85c0a18937c7c3f21d15269280b374dcf70
Joris de Ruiter <joris@stay-bold.com>
2018-04-06 12:46:16 +0200

    Add misfits

---
+++ src/components/ProfileCard/ProfileCard.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Button } from 'offcourse-ui-components';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+
+const propTypes = {
+  profile: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    tagline: PropTypes.string.isRequired,
+    skills: PropTypes.arrayOf(PropTypes.string).isRequired,
+    image: PropTypes.object.isRequired,
+  }).isRequired,
+};
+
+const ProfileCard = (props) => {
+  const { profile } = props;
+
+  return (
+    <div className="ui card">
+      <section
+        section="header"
+        className="content"
+      >
+        <h1 section="header" className="header">{profile.tagline}</h1>
+      </section>
+
+      <section
+        section="meta"
+        className="content"
+      >
+
+        {profile.image && profile.image.sizes &&
+          <div className="image">
+            <Img
+              sizes={profile.image.sizes}
+            />
+          </div>
+        }
+
+        <div className="meta">
+          Misfit
+        </div>
+
+        <div className="header">
+          <a className="curator-info" href="#">{profile.name}</a>
+        </div>
+      </section>
+
+      <section
+        section="tags"
+        className="content"
+      >
+        <div className="ui labels">
+          {profile.skills.map(skill => (
+            <a href="#" className="ui mini label">{skill}</a>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+ProfileCard.propTypes = propTypes;
+
+export default ProfileCard;
--- src/pages/join.js
+++ src/pages/join.js
@@ -4,14 +4,80 @@ import { Button } from 'offcourse-ui-components';
 import Helmet from 'react-helmet';
 import { navigateTo } from 'gatsby-link';
 
-import Avatars from '../components/Avatars';
 import CallToAction from '../components/CallToAction';
 import Quote from '../components/Quote';
 import PageHeader from '../components/PageHeader';
+import ProfileCard from '../components/ProfileCard';
 import Section from '../components/Section';
 
 
-const JoinPage = () => {
+const JoinPage = (props) => {
+  const { data } = props;
+  const misfits = [
+    {
+      name: 'Jan Hein Hoogstad',
+      tagline: 'Maker and Chief',
+      skills: ['programming', 'entrepreneurship', 'problem solving', 'critical thinking'],
+      image: data.yeehaa,
+    },
+
+    {
+      name: 'Birgit Vene',
+      tagline: 'Creator of Opportunities',
+      skills: ['project management', 'collaboration', 'critical thinking', 'community building'],
+      image: data.birgit,
+    },
+
+    {
+      name: 'Sander Reukema',
+      tagline: 'Reasoner',
+      skills: ['data science', 'finance'],
+      image: data.sander,
+    },
+
+    {
+      name: 'Kelly Sinterniklaas',
+      tagline: 'Ambassador of Buzz',
+      skills: ['marketing', 'communicatie', 'copywriting'],
+      image: data.kelly,
+    },
+
+    {
+      name: 'Charlotte van Oostrum',
+      tagline: 'Identifier of Companionships',
+      skills: ['strategy consulting', 'community development', 'content management'],
+      image: data.charlotte,
+    },
+
+    {
+      name: 'Christine Furst',
+      tagline: 'Visualizer',
+      skills: ['photography'],
+      image: data.christine,
+    },
+
+    {
+      name: 'Loek Hendriks',
+      tagline: 'Enthusiastic Engineer ',
+      skills: ['industrial engineering'],
+      image: data.loek,
+    },
+
+    {
+      name: 'Martijn Howling',
+      tagline: 'Pretty Maker',
+      skills: ['web design', 'entrepreneurship'],
+      image: data.martijn,
+    },
+
+    {
+      name: 'Joris de Ruiter',
+      tagline: 'Creator of Things',
+      skills: ['web & app development', 'entrepreneurship', 'online marketing'],
+      image: data.joris,
+    },
+  ];
+
   return (
     <div>
       <Helmet
@@ -65,11 +131,95 @@ const JoinPage = () => {
         </div>
       </Section>
       <Section
+        backgroundColor="offwhite"
         title="Our Band of Misfits"
         subtitle="We are a group of people who share the same beliefs, not necessarily the same (educational) backgrounds or levels. What we have in common is that we are a band of misfits - we have a different view on education and learning from the traditional system."
-      />
+      >
+
+        <div className="ui middle aligned grid container">
+          <div className="centered row">
+            <div className="ten wide column">
+              <h2>Each with their own set of skills</h2>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="ui stackable grid container">
+          <div className="row">
+            {misfits.map((misfit, index) => {
+              return (
+                <div
+                  key={index}
+                  className="sixteen wide mobile eight wide tablet four wide computer column"
+                  style={{
+                    // Make font sizes in CourseCard smaller to match app's sizes
+                    fontSize: '0.7em',
+                    marginBottom: '1rem',
+                  }}
+                >
+                  <ProfileCard profile={misfit} />
+                </div>
+
+              );
+            })}
+
+          </div>
+        </div>
+
+      </Section>
     </div>
   );
 };
 
 export default JoinPage;
+
+export const query = graphql`
+  query JoinImageQuery {
+    yeehaa: imageSharp(id: { regex: "/../assets/img/misfits/yeehaa.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    birgit: imageSharp(id: { regex: "/../assets/img/misfits/birgit.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    sander: imageSharp(id: { regex: "/../assets/img/misfits/sander.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    kelly: imageSharp(id: { regex: "/../assets/img/misfits/kelly.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    charlotte: imageSharp(id: { regex: "/../assets/img/misfits/charlotte.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    christine: imageSharp(id: { regex: "/../assets/img/misfits/christine.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    loek: imageSharp(id: { regex: "/../assets/img/misfits/loek.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    martijn: imageSharp(id: { regex: "/../assets/img/misfits/martijn.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    joris: imageSharp(id: { regex: "/../assets/img/misfits/joris.jpg/" }) {
+      sizes(maxWidth: 300) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+  }
+`;
