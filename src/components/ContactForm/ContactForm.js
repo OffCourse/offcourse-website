e70d0769c2ad962ac5ee664b3e5f0de35554cf12
Joris de Ruiter <joris@stay-bold.com>
2018-04-06 11:34:49 +0200

    Add contact form submit handling

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,77 +1,42 @@
 import React, { Component } from 'react';
 import { Button } from 'offcourse-ui-components';
 
-class ContactForm extends Component {
-  constructor(props) {
-    super(props);
+const ContactForm = () => {
+  return (
+    <form
+      className="ui form"
+      method="post"
+      action="https://formspree.io/contact@offcourse.io"
+    >
+      <div className="field text-left">
+        <label htmlFor="name">Name</label>
+        <input
+          name="name"
+          id="name"
+          type="text"
+        />
+      </div>
+      <div className="field text-left">
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          id="email"
+          type="email"
+        />
+      </div>
+      <div className="field text-left">
+        <label htmlFor="message">Message</label>
+        <textarea
+          name="message"
+          id="message"
+        />
+      </div>
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-    this.state = {
-      email: '',
-      message: '',
-      name: '',
-    };
-  }
-
-  handleInputChange(ev) {
-    const { name, value } = ev.target;
-
-    this.setState({ [name]: value });
-  }
-
-  handleSubmit() {
-    alert('We need to submit contact form');
-
-    this.setState({
-      email: '',
-      message: '',
-      name: '',
-    });
-  }
-
-  render() {
-    const { email, message, name } = this.state;
-
-    return (
-      <form className="ui form" onSubmit={this.handleSubmit}>
-        <div className="field text-left">
-          <label htmlFor="name">Name</label>
-          <input
-            name="name"
-            id="name"
-            onChange={this.handleInputChange}
-            type="text"
-            value={name}
-          />
-        </div>
-        <div className="field text-left">
-          <label htmlFor="email">Name</label>
-          <input
-            name="email"
-            id="email"
-            onChange={this.handleInputChange}
-            type="email"
-            value={email}
-          />
-        </div>
-        <div className="field text-left">
-          <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            onChange={this.handleInputChange}
-            value={message}
-          />
-        </div>
-
-        <div className="text-left">
-          <Button type="submit">Submit message</Button>
-        </div>
-      </form>
-    );
-  }
-}
+      <div className="text-left">
+        <Button type="submit">Submit message</Button>
+      </div>
+    </form>
+  );
+};
 
 export default ContactForm;
