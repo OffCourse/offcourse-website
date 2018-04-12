5cda598fc124a29939e595c32cc79b3affe9e55c
Joris de Ruiter <joris@stay-bold.com>
2018-04-12 08:35:11 +0200

    :construction: Improve AuthTabs styles

--- src/components/AuthTabs/AuthTabs.js
+++ src/components/AuthTabs/AuthTabs.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { AuthProvider, Button, Card, Label, Header, SignInForm, SignUpForm } from 'offcourse-ui-components';
+import {
+  AuthProvider,
+  Button,
+  Card,
+  Label,
+  Header,
+  SignInForm,
+  SignUpForm,
+} from 'offcourse-ui-components';
 
 import './style.less';
 
@@ -18,76 +26,82 @@ class AuthTabs extends Component {
 
     return (
       <div className="AuthTabs">
-        <div className="ui secondary pointing menu">
-          <a
-            className={`${ activeMenuItem === 'signUp' ? 'active ' : ''}item`}
-            onClick={() => { this.setState({ activeMenuItem: 'signUp' }); }}
-          >
-            Sign Up
-          </a>
-          <a
-            className={`${ activeMenuItem === 'signIn' ? 'active ' : ''}item`}
-            onClick={() => { this.setState({ activeMenuItem: 'signIn' }); }}
-          >
-            Sign In
-          </a>
+        <div className="AuthTabs__menu">
+          <div className="ui secondary pointing menu">
+            <a
+              className={`${ activeMenuItem === 'signUp' ? 'active ' : ''}item`}
+              onClick={() => { this.setState({ activeMenuItem: 'signUp' }); }}
+            >
+              Sign Up
+            </a>
+            <a
+              className={`${ activeMenuItem === 'signIn' ? 'active ' : ''}item`}
+              onClick={() => { this.setState({ activeMenuItem: 'signIn' }); }}
+            >
+              Sign In
+            </a>
+          </div>
         </div>
 
         <div className="ui segment">
-          {activeMenuItem === 'signUp' &&
-            <AuthProvider onSignIn={handler} onSignOut={handler}>
-              {({ signUp, signOut, authData }) => {
-                const {
-                  authStatus,
-                  errors,
-                  userName,
-                  needsConfirmation
-                } = authData;
-
-                return authStatus !== 'SIGNED_IN' ? (
-                  <SignUpForm
-                    confirmMode={needsConfirmation}
-                    errors={errors}
-                    onSubmit={signUp}
-                  />
-                ) : (
-                  <Card>
-                    <div>
-                      <h1>Signed in</h1>
-                      <Label>User Name</Label>
-                      <Header>{userName}</Header>
-                    </div>
-                    <div>
+          <div className="AuthTabs__item">
+            {activeMenuItem === 'signUp' &&
+              <AuthProvider onSignIn={handler} onSignOut={handler}>
+                {({ signUp, signOut, authData }) => {
+                  const {
+                    authStatus,
+                    errors,
+                    userName,
+                    accessToken,
+                    needsConfirmation,
+                    refreshToken
+                  } = authData;
+                  return authStatus !== "SIGNED_IN" ? (
+                    <SignUpForm
+                      confirmMode={needsConfirmation}
+                      errors={errors}
+                      onSubmit={signUp}
+                    />
+                  ) : (
+                    <Card>
+                      <div>
+                        <Label>User Name</Label>
+                        <Header>{userName}</Header>
+                      </div>
+                      <div>
+                        <Button onClick={signOut}>Sign Out</Button>
+                      </div>
+                    </Card>
+                  );
+                }}
+              </AuthProvider>
+            }
+            {activeMenuItem === 'signIn' &&
+              <AuthProvider onSignIn={handler} onSignOut={handler}>
+                {({ signIn, signOut, authData }) => {
+                  const {
+                    authStatus,
+                    errors,
+                    userName,
+                    accessToken,
+                    refreshToken
+                  } = authData;
+                  return authStatus !== "SIGNED_IN" ? (
+                    <SignInForm errors={errors} onSubmit={signIn} />
+                  ) : (
+                    <Card>
+                      <div>
+                        <Label>User Name</Label>
+                        <Header>{userName}</Header>
+                      </div>
                       <Button onClick={signOut}>Sign Out</Button>
-                    </div>
-                  </Card>
-                );
-              }}
-            </AuthProvider>
-          }
-          {activeMenuItem === 'signIn' &&
-            <AuthProvider onSignIn={handler} onSignOut={handler}>
-              {({ signIn, signOut, authData }) => {
-                const {
-                  authStatus,
-                  errors,
-                  userName,
-                } = authData;
+                    </Card>
+                  );
+                }}
+              </AuthProvider>
 
-                return authStatus !== 'SIGNED_IN' ? (
-                  <SignInForm errors={errors} onSubmit={signIn} />
-                ) : (
-                  <Card>
-                    <div>
-                      <Label>User Name</Label>
-                      <Header>{userName}</Header>
-                    </div>
-                    <Button onClick={signOut}>Sign Out</Button>
-                  </Card>
-                );
-              }}
-            </AuthProvider>
-          }
+            }
+          </div>
         </div>
       </div>
     );
