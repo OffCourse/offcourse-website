eb00d880b656b2171dffefabe9e4cbf4dd000909
Joris de Ruiter <joris@stay-bold.com>
2018-04-13 18:37:07 +0200

    Add mailchimp integration on newsletter sign up

--- src/components/NewsletterSignUp/NewsletterSignUp.js
+++ src/components/NewsletterSignUp/NewsletterSignUp.js
@@ -1,47 +1,49 @@
 import React, { Component } from 'react';
 import { Button } from 'offcourse-ui-components';
+import validator from 'email-validator';
 
 class NewsletterSignUp extends Component {
   constructor(props) {
     super(props);
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
 
     this.state = {
       email: '',
     };
   }
 
-  handleInputChange(ev) {
-    const { name, value } = ev.target;
+  handleEmailChange(ev) {
+    const { value } = ev.target;
 
-    this.setState({ [name]: value });
-  }
-
-  handleSubmit() {
-    const { email } = this.state;
-
-    alert(`We need to submit ${email} to mailing list`);
-
-    this.setState({ email: '' });
+    this.setState({ email: value });
   }
 
   render() {
     const { email } = this.state;
 
+    const isValid = validator.validate(email);
+
     return (
-      <div className="ui large action input">
-        <input
-          id="email"
-          name="email"
-          onChange={this.handleInputChange}
-          placeholder="E-mail"
-          type="text"
-          value={email}
-        />
-        <Button positive onClick={this.handleSubmit}>JOIN</Button>
-      </div>
+      <form
+        className="ui form"
+        action="https://offcourse.us13.list-manage.com/subscribe/post?u=3888907a37a3a88f5d6d88573&amp;id=3009ed104a"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+      >
+        <div className="ui large action input">
+          <input
+            id="email"
+            name="EMAIL"
+            onChange={this.handleEmailChange}
+            placeholder="E-mail"
+            type="text"
+            value={email}
+          />
+          <Button positive type="submit" disabled={!isValid}>JOIN</Button>
+        </div>
+      </form>
     );
   }
 }
